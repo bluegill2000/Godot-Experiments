d7469abf45ac4039e242111aafbe5b88bf284ff8
James Cooper <jlcaeo@att.net>
2024-10-19 20:42:53 -0500

    Create prefabs for environment objects

--- Map Generation/scripts/tile.gd
+++ Map Generation/scripts/tile.gd
@@ -1,5 +1,13 @@
 extends Node3D
 
+const pine_tree_1_scene = preload("res://prefabs/pine_forest/pine_tree_1.tscn")
+const pine_tree_2_scene = preload("res://prefabs/pine_forest/pine_tree_2.tscn")
+const pine_tree_3_scene = preload("res://prefabs/pine_forest/pine_tree_3.tscn")
+const pine_tree_4_scene = preload("res://prefabs/pine_forest/pine_tree_4.tscn")
+const fallen_log_1_scene = preload("res://prefabs/pine_forest/fallen_log_1.tscn")
+const rock_1_scene = preload("res://prefabs/pine_forest/rock_1.tscn")
+const rock_2_scene = preload("res://prefabs/pine_forest/rock_2.tscn")
+
 const tileSize = 10
 
 # Called when the node enters the scene tree for the first time.
@@ -7,14 +15,10 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func _process(delta):
+	#pass
 
 func configure(mapSeed: int, tile_type: Globals.WorldType, x: int, z: int):
-	# Clean tile
-	for child in get_children():
-		child.queue_free()
-	
 	position.x = x * tileSize
 	position.z = z * tileSize
 	
@@ -24,54 +28,53 @@ func configure(mapSeed: int, tile_type: Globals.WorldType, x: int, z: int):
 	if tile_type == Globals.WorldType.pine_forest:
 		# Trees 1-4
 		for i in generator.randi_range(1, 4):
-			var tree_texture_number = generator.randi_range(1, 4)
-			var tree_node = Sprite3D.new()
-			var image = Image.new()
-			image.load("res://assets/pine forest/pine_tree_" + str(tree_texture_number) + ".png")
-			tree_node.texture = ImageTexture.create_from_image(image)
+			var tree_node: Node3D
+			
+			match generator.randi_range(1, 4):
+				1:
+					tree_node = pine_tree_1_scene.instantiate()
+				2:
+					tree_node = pine_tree_2_scene.instantiate()
+				3:
+					tree_node = pine_tree_3_scene.instantiate()
+				4:
+					tree_node = pine_tree_4_scene.instantiate()
+			
 			tree_node.position.x = generator.randf_range(0, tileSize)
 			tree_node.position.z = generator.randf_range(0, tileSize)
 			
 			# Scale the trees
-			tree_node.pixel_size *= generator.randf_range(0.7, 1.3)
-			tree_node.billboard = BaseMaterial3D.BILLBOARD_FIXED_Y
-			
-			var image_height = tree_node.texture.get_height() * tree_node.pixel_size
-			tree_node.position.y = image_height / 2
+			tree_node.find_child("tree_sprite").pixel_size *= generator.randf_range(0.7, 1.3)
 			
 			add_child(tree_node)
 		
 		# Rocks 0-1
 		if generator.randi() % 2:
-			var rock_texture_number = generator.randi_range(1, 2)
-			var rock_node = Sprite3D.new()
-			var image = Image.new()
-			image.load("res://assets/pine forest/rock_" + str(rock_texture_number) + ".png")
-			rock_node.texture = ImageTexture.create_from_image(image)
+			var rock_node: Node3D
+			match generator.randi_range(1, 2):
+				1:
+					rock_node = rock_1_scene.instantiate()
+				2:
+					rock_node = rock_2_scene.instantiate()
+			
 			rock_node.position.x = generator.randf_range(0, tileSize)
 			rock_node.position.z = generator.randf_range(0, tileSize)
 			
 			# Shrink rocks down
-			rock_node.pixel_size = rock_node.pixel_size * 0.3
-			rock_node.billboard = BaseMaterial3D.BILLBOARD_FIXED_Y
-			var image_height = rock_node.texture.get_height() * rock_node.pixel_size
-			rock_node.position.y = image_height / 2
+			rock_node.scale *= generator.randf_range(0.3, 0.5)
+			
+			# Rotate rocks
+			rock_node.rotate_y(deg_to_rad(generator.randi_range(0, 359)))
 			
 			add_child(rock_node)
 		
 		# Fallen logs 0-1
 		if generator.randi() % 2:
-			var log_node = Sprite3D.new()
-			var image = Image.new()
-			image.load("res://assets/pine forest/fallen_log_1.png")
-			log_node.texture = ImageTexture.create_from_image(image)
+			var log_node = fallen_log_1_scene.instantiate()
 			log_node.position.x = generator.randf_range(0, tileSize)
 			log_node.position.z = generator.randf_range(0, tileSize)
 			
 			# Rotate log
-			log_node.rotate_z(deg_to_rad(-45))
-			log_node.billboard = BaseMaterial3D.BILLBOARD_FIXED_Y
-			var image_height = log_node.texture.get_height() * log_node.pixel_size
-			log_node.position.y = image_height / 2
+			log_node.rotate_y(deg_to_rad(generator.randi_range(0, 359)))
 			
 			add_child(log_node)
--- Map Generation/scripts/tile_manager.gd
+++ Map Generation/scripts/tile_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var tileContainer: Node
 
+const tile_scene = preload("res://prefabs/pine_forest/pine_forest_tile.tscn")
 const rowOffset: int = -4
 const colOffset: int = -4
 const rows: int = 6
@@ -15,34 +16,10 @@ var tiles: Array[Sprite3D] = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Generate tile set
-	var script = load(tileScriptPath)
 	var worldSeed: int = randi()
-	var counter := 1
 	for row in range(rowOffset, rowOffset + rows):
 		for col in range(colOffset, colOffset + cols):
-			var tile = Sprite3D.new()
-			tile.set_script(script)
-			
-			# Tile contents
-			tile.configure(worldSeed, Globals.WorldType.pine_forest, row, col)
-			
-			# Tile background
-			var tileImage = Sprite3D.new()
-			var image = Image.new()
-			image.load(tileTexturePath)
-			var texture = ImageTexture.create_from_image(image)
-			var textureSize = texture.get_size()
-			tileImage.axis = Vector3.AXIS_Y
-			tileImage.texture = texture
-			tileImage.scale = Vector3(1000 / textureSize.x, 1, 1000 / textureSize.y)
-			tileImage.position.x = 5
-			tileImage.position.z = 5
-			tile.add_child(tileImage)
+			var tile = tile_scene.instantiate()
 			
 			tileContainer.add_child(tile)
-			counter += 1
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+			tile.configure(worldSeed, Globals.WorldType.pine_forest, row, col)
